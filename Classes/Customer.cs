24eba6b48f65e7514ede85efdfb4cbab9b6ad7fd
aydindemir1 <aynndemr@gmail.com>
2024-02-18 15:16:51 +0300

    Add project files.

---
+++ Classes/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+
+
+        public int Id { get; set; }
+
+        public int FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
