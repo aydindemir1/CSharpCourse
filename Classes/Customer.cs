f191e1817c5500e310af447d306aee82966b0948
aydindemir1 <aynndemr@gmail.com>
2024-05-01 00:00:02 +0300

    day 1

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -12,7 +12,7 @@ namespace Classes
 
         public int Id { get; set; }
 
-        public int FirstName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
     }
